52540ba6cbceccc9e8742dc1621755725481c83a
maoz na <you@example.com>
2021-09-03 19:58:02 +0300

    ters1

--- app.js
+++ app.js
@@ -273,6 +273,6 @@ app.post("/submit",function(req,res){
     }
   })
 })
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
 });
