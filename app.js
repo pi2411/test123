a0a96817f2703e8f6ba1e03ec0a24f29ea9b7853
maoz na <you@example.com>
2021-09-03 19:28:33 +0300

    ters1

--- app.js
+++ app.js
@@ -273,6 +273,7 @@ app.post("/submit",function(req,res){
     }
   })
 })
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
