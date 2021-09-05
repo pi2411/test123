24b8132639bf0816efdcf9ddc7ee9b9238a753c6
maoz na <you@example.com>
2021-09-05 21:07:08 +0300

    my close_to_finish_tow

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require('passport-local-mongoose');
- // const cookieParser = require('cookie-parser');
+  const cookieParser = require('cookie-parser');
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const facebookStrategy = require('passport-facebook').Strategy
 const findOrCreate = require('mongoose-findorcreate');
@@ -26,7 +26,7 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-   // app.use(cookieParser());
+    app.use(cookieParser());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
   extended: true
