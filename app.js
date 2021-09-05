253eb11495e24e8b4142b808ec1f7725681ed0d7
maoz na <you@example.com>
2021-09-06 00:04:23 +0300

    make it better1

--- app.js
+++ app.js
@@ -115,12 +115,12 @@ passport.deserializeUser(function(id, done) {
 
 app.get('/profile', isLoggedIn, function(req, res) {
     console.log(req.user)
-    res.json({
-        message:"You have accessed the protected endpoint!",
-        yourUserInfo : req.user,
-      });
-    // res.render('profile', {user : req.user // get the user out of session and pass to template
+    // res.json({
+    //     message:"You have accessed the protected endpoint!",
+    //     yourUserInfo : req.user,
     //   });
+    res.render('profile', {user : req.user // get the user out of session and pass to template
+      });
 });
 
 // route middleware to make sure
