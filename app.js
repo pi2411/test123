7940e43c58dc7074bd1e433cc284117c689c824c
maoz na <you@example.com>
2021-09-05 22:15:24 +0300

    my close_to_finish_3num

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cookieParser());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
   extended: true
@@ -115,10 +116,12 @@ passport.deserializeUser(function(id, done) {
 
 app.get('/profile', isLoggedIn, function(req, res) {
     console.log(req.user)
-    res.render('profile', {
-        user : req.user // get the user out of session and pass to template
-    });
+    res.json({
+        message:"You have accessed the protected endpoint!",
+        yourUserInfo : req.user,
+      });
 });
+
 // route middleware to make sure
 function isLoggedIn(req, res, next) {
 
