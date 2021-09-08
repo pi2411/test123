930dce7114459af4a8f6c06cd0f544ece066e0e0
maoz na <you@example.com>
2021-09-08 19:04:21 +0300

    progile err

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ passport.use(new GoogleStrategy({
     clientID:     process.env.CLINET_ID,
     clientSecret: process.env.CLINTE_SECRET,
     callbackURL: "https://stark-caverns-47144.herokuapp.com/auth/google/profile",
-    // profileFields   : ['id','displayName','name','gender','picture.type(large)','email'],
-    passReqToCallback   : true
+    profileFields   : ['id','displayName','name','gender','picture.type(large)','email'],
+
   },
-  function(request,accessToken, refreshToken, profile, cb) {
+  function(accessToken, refreshToken, profile, cb) {
          User.findOrCreate({uid: profile.id}, function (err, user) {
            console.log('A new uxer from "%s" was inserted', user.uid);
            if(user){
