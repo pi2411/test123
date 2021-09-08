b43caa0092580cf81994ae11b24362dc819b371d
maoz na <you@example.com>
2021-09-08 15:34:41 +0300

    finish test1 08/09/2021

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const passportLocalMongoose = require('passport-local-mongoose');
-const findOrCreate = require('mongoose-findorcreate');
+// const passportLocalMongoose = require('passport-local-mongoose');
+// const findOrCreate = require('mongoose-findorcreate');
 mongoose.connect("mongodb+srv://admin_ygal:test123@cluster0.es7mi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -12,6 +12,6 @@ const postSchema = new mongoose.Schema({
   title:String,
   content:String,
 })
-postSchema.plugin(passportLocalMongoose);
-postSchema.plugin(findOrCreate);
+// postSchema.plugin(passportLocalMongoose);
+// postSchema.plugin(findOrCreate);
  module.exports = mongoose.model("post",postSchema);
